bc5ff19b230a5bbe76541446a46e0db812e60c57
v3rena <v.poetzl@gmx.at>
2017-10-05 15:43:33 +0200

    Program.cs refactored

--- ECard/Program.cs
+++ ECard/Program.cs
@@ -11,27 +11,36 @@ namespace ECard
 	{
 		static void Main(string[] args)
 		{
-			var b = new BaseService.BaseService();
-			b.Url = "https://10.196.4.114/base/15";
+			var b = new BaseService.BaseService
+			{
+				Url = "https://10.196.4.114/base/15"
+			};
 
+			AuthenticateCard(b);
+
+			Console.WriteLine("Alles super."); Console.ReadKey();
+		}
+
+		public static void AuthenticateCard(BaseService.BaseService b)
+		{
 			var readers = b.getCardReaders();
 			var reader = readers[0];
 
-			var prodInfo = new produktInfo();
-			prodInfo.produktId = 900101;
-			prodInfo.produktVersion = "0.0.1";
-			prodInfo.produktIdSpecified = true;
+			var prodInfo = new produktInfo()
+			{
+				produktId = 900101,
+				produktVersion = "0.0.1",
+				produktIdSpecified = true
+			};
 
-			var dialog = b.createDialog(reader.id, prodInfo, "", true, true);
-			
+			string dialog = b.createDialog(reader.id, prodInfo, "", true, true);
+
 			var vp = b.authenticateDialog(dialog, "", "1122", reader.id);
 			var tb = vp.ordination[0].taetigkeitsBereich;
 
-			b.setDialogAddress(dialog, vp.ordination[0].ordinationId, tb[0].id, "700",  new gdaMa(), "");
-
+			b.setDialogAddress(dialog, vp.ordination[0].ordinationId, tb[0].id, "700", new gdaMa(), "");
+	
 			b.closeDialog(dialog);
-			Console.WriteLine("Alles super.");
-			Console.ReadKey();
 		}
 	}
 }
