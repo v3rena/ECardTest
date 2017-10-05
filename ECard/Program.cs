652a770eb488b899cb72d9e1b3ec0c81b2371c19
v3rena <v.poetzl@gmx.at>
2017-10-05 15:35:47 +0200

    O-Card Auth

---
+++ ECard/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECard.BaseService;
+
+namespace ECard
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var b = new BaseService.BaseService();
+			b.Url = "https://10.196.4.114/base/15";
+
+			var readers = b.getCardReaders();
+			var reader = readers[0];
+
+			var prodInfo = new produktInfo();
+			prodInfo.produktId = 900101;
+			prodInfo.produktVersion = "0.0.1";
+			prodInfo.produktIdSpecified = true;
+
+			var dialog = b.createDialog(reader.id, prodInfo, "", true, true);
+			
+			var vp = b.authenticateDialog(dialog, "", "1122", reader.id);
+			var tb = vp.ordination[0].taetigkeitsBereich;
+
+			b.setDialogAddress(dialog, vp.ordination[0].ordinationId, tb[0].id, "700",  new gdaMa(), "");
+
+			b.closeDialog(dialog);
+			Console.WriteLine("Alles super.");
+			Console.ReadKey();
+		}
+	}
+}
